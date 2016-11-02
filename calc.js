878c82a142beaf4b81eb6cdc5ff0fe85758db232
Arkkro <djarkkro@gmail.com>
2016-11-02 15:19:11 -0700

    Calc stuff and fixes

---
+++ calc.js
@@ -0,0 +1,19 @@
+var isLoaded="Loaded"
+
+function init(){
+  startTime();
+  console.log(isLoaded)
+}
+
+
+
+var inp1 = document.getElementById('input1');
+var inp2 = document.getElementById('input2');
+var result;
+
+function compute(){
+  var inp1 + inp2 = result;
+  var result = resultText;
+  display.innerHTML = resultText;
+}
+console.log(resultText)
