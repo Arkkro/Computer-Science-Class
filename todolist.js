ffeb222fe371976535fddae227012bdfe6f6b414
Arkkro <djarkkro@gmail.com>
2016-11-08 15:25:46 -0800

    aoeuidht

--- todolist.js
+++ todolist.js
@@ -54,14 +54,12 @@ var addButton = document.getElementById("addButton");
 var select = document.getElementById("select");
 
 addButton.addEventListener("click", function(){
-    var task = "<p>" + input.value + "</p>" + "<br>"
-
+    var task = "<div>" + input.value + "</div>" + "<br id="taskBr">"
     taskItem.innerHTML = taskItem.innerHTML + task;
-
-
+    console.log("");
 });
-// 
-// delet.addEventListener("click," function(evt){
-//   var thing = evt.target
-//   thing.parentNode.removeChild(thing)
+
+// taskItem.addEventListener("click", function(evt){
+//   var removeItem = evt.target;
+//   removeItem.parentNode.removeChild(removeItem);
 // });
