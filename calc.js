7ef3bfb3bd47a33ca6fe337e14b83b9bf632aa3c
Arkkro <djarkkro@gmail.com>
2016-11-15 16:11:20 -0800

    comments

--- calc.js
+++ calc.js
@@ -38,41 +38,40 @@ function startTime() {
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('clocktext').innerHTML =
-    weekday+ " " +month + "/"+ date + "/" + year+ " " + " " + h + ":" + m + ":" + s;
+    weekday + " " + month + "/" + date + "/" + year + " " + " " + h + ":" + m + ":" + s;
     var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
-// // //
-// function test(){
-//   alert("Test Success");
-// }
-//
-// // //
+
+// // // // // //
 
 // // //
-// Gets elements by id so they can be refrenced by javascript
+// gets elements by id so they can be refrenced by javascript
 var computeButton = document.getElementById('computeButton');
-var input1 = document.getElementById('input1');
-var input2 = document.getElementById('input2');
-var result;
+var input1 = document.getElementById('input1'); // Type input box one
+var input2 = document.getElementById('input2'); // Type input box two
+var result; // creates a varible to hold
 // varible result is created to store the answer
 // // //
 
 // // //
-// creates 
+// creates a listener that waits for a "click" on the computeButton element
 computeButton.addEventListener("click", compute);
+// when clicked it will call the function compute
+// // //
 
-
+// // //
+// compute function that gives the answer of selected operator on the given numbers
 function compute(){
-  var selectOp = document.getElementById('selectOp');
-  var int1 = parseInt(input1.value);
-  var int2 = parseInt(input2.value);
+  var selectOp = document.getElementById('selectOp'); // gets element of the operator selecter
+  var int1 = parseInt(input1.value); // creates a varible that = a string of the first input box and converts it into a number that javascript can do math to it
+  var int2 = parseInt(input2.value); // creates a varible that = a string of the second input box and converts it into a number that javascript can do math to it
 
-  if (selectOp.value === "+") {
-    var result = int1 + int2;
+  if (selectOp.value === "+") { // checks if the plus operation is selected and executes if it is
+    var result = int1 + int2; // i
     alert ("Your answer is "  + result)
   }
   else if (selectOp.value === "-") {
@@ -97,6 +96,6 @@ function compute(){
     alert ("Your answer is "  + result)
   }
   else {
-    alert ("Injecting Javascript I see")
+
   }
   }
